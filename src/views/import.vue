7010476d37a3577ce6aa70311ccec0cee90b8553
1linyan <1>
2023-05-31 17:58:22 +0800

    侧边栏

--- src/views/import.vue
+++ src/views/import.vue
@@ -4,59 +4,135 @@
             <div class="card-header">
                 <el-button class="button" type="primary">添加分类</el-button>
             </div>
-            
+
         </template>
         <div>
-                <tables :tableModels="tableList" :tableData="tableData" @click_1="test1" @click_2="test2" @click_3="test3"></tables>
-            </div>
+            <el-table :data="tableList" border class="table" header-cell-class-name="table-header">
+                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+                <el-table-column prop="name" label="设备名称" align="center"></el-table-column>
+                <el-table-column prop="status" label="状态" align="center"></el-table-column>
+                <el-table-column prop="image" label="图片" align="center">
+                    <template #default="scope">
+                        <el-image style="width: 60px;height: 60px;" :src="scope.row.image"
+                            :preview-src-list="[scope.row.image]" hide-on-click-modal="true" preview-teleported="true">
+                            <template #error>
+                                <div class="image-slot">
+                                    <el-icon>
+                                        <user />
+                                    </el-icon>
+                                </div>
+                            </template>
+                        </el-image>
+                    </template>
+                </el-table-column>
+                <el-table-column fixed="right" label="操作" align="center">
+                    <template #default="scope">
+                        <el-button link type="primary" :icon="Edit" @click="handleEdit(scope.row, scope.$index)">编辑
+                        </el-button>
+                        <el-popconfirm title="确定要删除吗？" @confirm="handleDelete(scope.row, scope.$index)"
+                            confirm-button-text="确定" cancel-button-text="再想想">
+                            <template #reference>
+                                <el-button link type="danger" :icon="Delete">删除
+                                </el-button>
+                            </template>
+                        </el-popconfirm>
+                        <slot name="operations" v-bind="scope.row"></slot>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <!-- <tables :tableModels="tableList" :tableData="tableData" @click_1="test1" @click_2="test2" @click_3="test3"></tables> -->
+        </div>
     </el-card>
 </template>
 
 <script setup lang="ts" name="import">
-import {ref} from "vue";
+import { ref } from "vue";
 import service from '@/utils/http.ts';
-const tableList =ref([]);
-// const tableData = {
-//     reqUrl: "https://easy-mock.com/mock/5f2403effa76424e9506a6b8/example/getData",
-//     operaData: {
-//         isShow: true,
-//         data: [
-//             { label: "删除", type: "danger" },
-//             { label: "增加", type: "primary" },
-//             { label: "其他", type: "info" }
-//         ]
-//     },
-//     columnData: [
-//         { prop: "name", label: "名字", width: 200, align: "center" },
-//         { prop: "time", label: "时间", align: "center" }
-//     ]
-// }
+import { Delete, Edit } from '@element-plus/icons-vue';
 
-// const test1 = (e: any) => {
-//     //   this.$message("删除")
-//     console.log("删除", e.name);
-// }
-// const test2 = (e: any) => {
-//     //   this.$message("增加");
-//     console.log("增加", e.name);
-// }
-// const test3 = (e: any) => {
-//     //   this.$message("其他");
-//     console.log("其他", e.name);
-// }
 
-const getData =()=>{
+
+// const getTime = (v: any, day:any) => {
+    
+
+//     /* 获取现在的年份 */
+//     let dateTimeNow = new Date().getFullYear();
+//     var month = new Date().getMonth() + 1;
+//     var days = new Date().getDate();
+//     /* 前一年 */
+//     let dateTimeBefore = new Date(new Date().setFullYear(dateTimeNow - 1)).getFullYear();
+//     if (v < dateTimeNow) {
+//         //年 月 日 时 分
+//         console.log("===小于当前年====>")
+//     }
+//     if (v == dateTimeNow) {
+//         if (days === day) {
+//             //  是当前年 同时是当天
+//             //  时分
+//         } else {
+//             //  月 日 时 分
+//         }
+//         console.log("===等于当前年====>", month)
+//     }
+
+//     console.log('dateTimeNow', dateTimeNow)
+//     console.log('dateTimeBefore', dateTimeBefore)
+//     console.log('days', days)
+
+// }
+// getTime(2023,day)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 初始化默认参数
+const props = withDefaults(defineProps(), {
+    total: 0,
+    showEditBtn: true,
+    showDeleteBtn: true,
+    operationsWidth: 200
+});
+console.log('------->>>', props.operationsWidth)
+
+const tableList = ref([]);
+
+const getData = () => {
     service('/lst', {
         method: 'post',
         data: {},
-      }).then((res: any) => {
-     tableList.value= res.data;
-        console.log("==res===>", tableList.value )
-      })
+    }).then((res: any) => {
+        tableList.value = res.data;
+        console.log("==res===>", tableList.value)
+    })
 }
-getData()
 
- 
+// 点击了编辑
+const handleEdit = (data: any, index: any) => {
+    console.log('handleEdit', data, index);
+};
+
+// 点击了删除
+const handleDelete = (data: any, index: any) => {
+    console.log('handleDelete', data, index);
+};
+// onload(()=>{
+//     getData()
+// })
+getData()
 </script>
 
 <style scoped></style>
